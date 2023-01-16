ed388af84cd3b11cbd43a3c7fd88526823d02c97
Sujan Shrestha <shresthasujan148@gmail.com>
2023-01-16 17:14:03 +0545

    verification

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,29 +2,41 @@ const model = require("../models");
 const dotenv = require("dotenv");
 const bcrypt = require("bcryptjs")
 const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken")
 
 dotenv.config();
 //creating user
 const create = (req, res) => {
   const newUser = {
+    email:req.body.email,
     name: req.body.name,
     contact: req.body.contact,
     password:req.body.password,
     gender: req.body.gender,
   };
-  if(req.body.password === req.body.confirmPassword){
+  if(newUser.password === req.body.confirmPassword){
     bcrypt.hash(newUser.password, 10, function(err, hash) {
       newUser.password=hash;
-      model.User.create(newUser)
-        .then((result) => {
+      model.User.findOne({where:{email:newUser.email}}).then((exist)=>{
+        if(exist){
           res.status(200).json({
-             newUser,
-            messege: "User created successful!",
-          });
-        })
-        .catch((err) => {
-          res.status(500).json({ messege: "Something went wrong", err: err });
-        });
+           messege: "Email already taken ",
+         });
+        }else{
+          model.User.create(newUser)
+            .then((result) => {
+              res.status(200).json({
+                 newUser,
+                messege: "User created successful!",
+              });
+            })
+            .catch((err) => {
+              res.status(500).json({ messege: "Something went wrong", err: err });
+            });
+        }
+      }).catch(err=>{
+        res.status(500).json({ messege: "Something went wrong", err: err });
+      })
   });
   }else{
     res.status(401).json({
@@ -35,14 +47,22 @@ const create = (req, res) => {
 
 //user login
 const login = (req, res) => {
-  model.User.findOne({ where: { name: req.body.name } })
+  model.User.findOne({ where: { email: req.body.email } })
   .then((user) => {
     if(user){
       bcrypt.compare(req.body.password,user.password,(err,result)=>{
         if(result){
-          res.status(200).json({
-            messege:"Login succcessful!"
-          })
+          const verify = jwt.sign(
+            {
+            user :user.name
+          },process.env.VERIFY_SEC,
+          (err, token) => {
+            res.status(200).json({
+              messege:"Login succcessful!",
+              token: token,
+            });
+          },{expiresIn:"1m "}
+          )
         }else{
           res.status(401).json({
             messege:"Invalid password!"
@@ -63,7 +83,99 @@ const login = (req, res) => {
     });
 };
 
+//update user
+const editUser = (req,res)=>{
+  model.User.findOne({ where: { email: req.body.email } }).then((exist)=>{
+    if(exist){
+      const editedUser ={
+        // email:req.body.email,
+        name: req.body.name,
+        contact: req.body.contact,
+        // password:req.body.password,
+      }
+      console.log(editUser)
+      model.User.update(editedUser,{where:{email:req.body.email}}).then((update)=>{
+        res.status(200).json({
+          messege:"user updated succcessfully!",
+          updated:editedUser,
+        })
+      }).catch(err=>{
+        res.status(500).json({
+          messege:"something went wrong!"
+        })
+      })
+    }else{
+      res.status(401).json({
+        messege:"user email not found"
+      })
+    }
+  }).catch(err=>{
+    res.status(500).json({
+      messege:"something went wrong!"
+    })
+  })
+}
+
+//delete user
+const deleteUser = (req, res) => {
+  model.User.destroy({ where: { email: req.body.email } })
+    .then((result) => {
+      if (result) {
+        res.status(200).json({
+          messege: `User  deleted`,
+        });
+      } else {
+        res.status(404).json({
+          messege: `User not found`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        messege: "Something went wrong",
+      });
+    });
+};
+
+//get all user
+const index = (req, res) => {
+  model.User.findAll()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        messege: "Something went wrong!!",
+      });
+    });
+};
+
+//get user by id
+const show = (req, res) => {
+  const id = req.params.id;
+
+  model.User.findByPk(id)
+    .then((result) => {
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        res.status(404).json({
+          messege: "Id not found",
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        messege: "Something went wrong!!",
+      });
+    });
+};
 module.exports = {
   create,
   login,
+  editUser,
+  deleteUser,
+  index,
+  show
+
 };
